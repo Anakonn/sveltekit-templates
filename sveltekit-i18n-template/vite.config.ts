f7db3b2c4f48fbf22478814610dd312a28e3640f
ego <mrego@anakonn.com>
2024-09-07 01:18:39 +0900

    i18n

--- sveltekit-i18n-template/vite.config.ts
+++ sveltekit-i18n-template/vite.config.ts
@@ -3,7 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),sveltekit()],
+	plugins: [paraglide({ project: './i18n.inlang', outdir: './src/lib/paraglide' }),sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
